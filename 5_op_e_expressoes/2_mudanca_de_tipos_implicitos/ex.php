9469f75972fc34547be8917cecd87ec80080ed88
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-01-25 18:46:27 -0300

    feat: expressoes e operadores - gettype

---
+++ 5_op_e_expressoes/2_mudanca_de_tipos_implicitos/ex.php
@@ -0,0 +1,12 @@
+<?php
+
+    $soma = "5" * 12;
+    
+    echo gettype($soma);
+    echo "<br>";
+    echo gettype([]);
+    echo "<br>";
+    echo gettype(12.2);
+    echo "<br>";
+    echo gettype("texto");
+
